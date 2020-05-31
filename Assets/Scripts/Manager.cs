53581a693c34693033a4f54ea0324b0b7dcc8de3
Dom <domportera@gmail.com>
2020-05-31 17:27:07 -0400

    Basic functionality finished

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,29 +8,63 @@ using UnityEngine.UI;
 
 public class Manager : MonoBehaviour
 {
-    [SerializeField] string saveLoadPath = null;
+    string saveLoadPath = "C:\\OneDrive\\Music Stuff\\Rapid MIDI Learn Presets";
 
     //save to this path when switching presets, load from it when saving or editing presets
     string rapidFilePath = "C:/ProgramData/Parawave/Rapid/midi_mapping.txt";
+    string fileExtension = ".txt";
 
     [SerializeField] Button saveButton = null;
     [SerializeField] Button loadButton = null;
+    [SerializeField] Button setPathButton = null;
+    [SerializeField] TMP_InputField pathField = null;
 
-    // Start is called before the first frame update
+    string pathSaveKey = "saveLoadPath";
+    
     void Start()
     {
-        //    saveAsInputField.onEndEdit.AddListener(PopulateListOfFolders);
-        saveButton.onClick.AddListener(() =>{ FileBrowser.ShowSaveDialog(OnSave, null, false, saveLoadPath); });
-        loadButton.onClick.AddListener(() => { FileBrowser.ShowLoadDialog(OnLoad, null, false, saveLoadPath); });
+        if(PlayerPrefs.HasKey(pathSaveKey))
+        {
+            saveLoadPath = PlayerPrefs.GetString(pathSaveKey);
+        }
+
+        pathField.text = saveLoadPath;
+
+        InitializeButtons();
+
+        setPathButton.onClick.AddListener(SetDefaultPath);
+
     }
 
     void OnSave(string _s)
     {
-        Debug.Log("save: " + _s);
+        string fileContents = File.ReadAllText(rapidFilePath);
+
+        if(!_s.Contains(fileExtension))
+        {
+            _s += fileExtension;
+        }
+
+        File.WriteAllText(_s, fileContents);
     }
     void OnLoad(string _s)
     {
-        Debug.Log("load: " + _s);
+        string fileContents = File.ReadAllText(_s);
+        File.WriteAllText(rapidFilePath, fileContents);
+    }
 
+    void SetDefaultPath()
+    {
+        Debug.Log(pathField.text);
+        PlayerPrefs.SetString(pathSaveKey, pathField.text);
+        saveLoadPath = pathField.text;
+    }
+
+    void InitializeButtons()
+    {
+        saveButton.onClick.RemoveAllListeners();
+        loadButton.onClick.RemoveAllListeners();
+        saveButton.onClick.AddListener(() => { FileBrowser.ShowSaveDialog(OnSave, null, false, saveLoadPath); });
+        loadButton.onClick.AddListener(() => { FileBrowser.ShowLoadDialog(OnLoad, null, false, saveLoadPath); });
     }
 }
